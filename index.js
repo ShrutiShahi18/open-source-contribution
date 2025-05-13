a1b80e1b23b47d450a503a450a13b8c04184b1fb
Shruti Shahi <yoshruti18@gmail.com>
2025-05-13 23:24:05 +0530

    Added some cool lines in js file

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("LMAO")
+// Generate a random hex color and log it
+const randomHexColor = () => `#${Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, '0')}`;
+console.log("Random Hex Color:", randomHexColor());
